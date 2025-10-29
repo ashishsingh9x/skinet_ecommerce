65cd46968011aa12b949fdd599b4bcec10d99cfd
Ashish Singh <ashishsingh.9x@gmail.com>
2025-10-29 12:21:07 +0530

    Added product CRUD operation.

---
+++ Infrastructure/Data/StoreContext.cs
@@ -0,0 +1,24 @@
+﻿using Core.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions options) : base(options)
+        {
+
+        }
+
+        public DbSet<Product> Products { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.Entity<Product>(entity =>
+            {
+                entity.Property(p => p.Price).HasColumnType("decimal(18,2)");
+            });
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}

8f2506469f0ca97314342b28b4b101670b871b99
Ashish Singh <ashishsingh.9x@gmail.com>
2025-10-29 20:39:17 +0530

    Implemented basic specification evaluator.

---
+++ Core/Specification/BaseSpecification.cs
@@ -0,0 +1,21 @@
+﻿using Core.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.Specification
+{
+    public class BaseSpecification<T> : ISpecification<T>
+    {
+        private readonly Expression<Func<T, bool>> criteria;
+        public BaseSpecification(Expression<Func<T, bool>> criteria)
+        {
+            this.criteria = criteria;
+        }
+
+        public Expression<Func<T, bool>> Criteria => criteria;
+    }
+}

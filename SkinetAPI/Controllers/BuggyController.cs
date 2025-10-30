9aca92b196dc657c8cec5434ead62bcf89b0a71b
Ashish Singh <ashishsingh.9x@gmail.com>
2025-10-30 17:20:47 +0530

    created buggy controller with different error

---
+++ SkinetAPI/Controllers/BuggyController.cs
@@ -0,0 +1,39 @@
+﻿using Core.Entities;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace SkinetAPI.Controllers
+{
+    public class BuggyController : BaseApiController
+    {
+        [HttpGet("unauthorized")]
+        public IActionResult GetUnauthorized()
+        {
+            return Unauthorized();
+        }
+
+        [HttpGet("badrequest")]
+        public IActionResult GetBadRequest()
+        {
+            return BadRequest("Not a good request");
+        }
+
+        [HttpGet("notfound")]
+        public IActionResult GetNotFound()
+        {
+            return NotFound();
+        }
+
+        [HttpGet("internalerror")]
+        public IActionResult GetInternalError()
+        {
+            throw new Exception("This is a test exception");
+        }
+
+        [HttpPost("validationerror")]
+        public IActionResult GetValidationError(Product product)
+        {
+            return Ok();
+        }
+    }
+}

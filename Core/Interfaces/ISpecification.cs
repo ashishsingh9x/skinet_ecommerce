be6690b79391e588decaa46e00da1c8236d2237b
Ashish Singh <ashishsingh.9x@gmail.com>
2025-10-30 11:29:44 +0530

    specfication to return string(TResult)

--- Core/Interfaces/ISpecification.cs
+++ Core/Interfaces/ISpecification.cs
@@ -8,4 +8,9 @@ namespace Core.Interfaces
         public Expression<Func<T, object>>? OrderBy { get; }
         public Expression<Func<T, object>>? OrderByDescending { get; }
     }
+
+    public interface ISpecification<T, TResult> : ISpecification<T>
+    {
+        public Expression<Func<T, TResult>>? Select { get; }
+    }
 }

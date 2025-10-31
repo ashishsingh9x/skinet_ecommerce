09cdeeebd95f430ea659cbbea3b2e873830f4a24
Ashish Singh <ashishsingh.9x@gmail.com>
2025-10-31 16:35:40 +0530

    Making basic http request to product api and showing in console

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, signal } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-root',
@@ -8,6 +9,17 @@ import { HeaderComponent } from "./layout/header/header.component";
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'Skinet';
+  baseUrl: string = "https://localhost:7006/api/";
+  constructor(private http: HttpClient) {
+  }
+
+  ngOnInit(): void { 
+    this.http.get(this.baseUrl + 'products').subscribe({
+      next: data => console.log(data),
+      error: error => console.log(error),
+      complete: () => console.log('complete')
+    });
+  }
 }

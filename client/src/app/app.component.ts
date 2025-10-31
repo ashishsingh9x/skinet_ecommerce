501d1fc814140ad7014a36b1f964687eb1f70fac
Ashish Singh <ashishsingh.9x@gmail.com>
2025-10-31 20:12:57 +0530

    Added product type and replaced any with product type.

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./layout/header/header.component";
 import { HttpClient } from '@angular/common/http';
+import { Product } from './shared/models/product';
+import { Pagination } from './shared/models/pagination';
 
 @Component({
   selector: 'app-root',
@@ -12,12 +14,12 @@ import { HttpClient } from '@angular/common/http';
 export class AppComponent implements OnInit {
   title = 'Skinet';
   baseUrl: string = "https://localhost:7006/api/";
-  products: any[] = [];
+  products: Product[] = [];
   constructor(private http: HttpClient) {
   }
 
   ngOnInit(): void { 
-    this.http.get<any>(this.baseUrl + 'products').subscribe({
+    this.http.get<Pagination<Product>>(this.baseUrl + 'products').subscribe({
       next: response => this.products = response.data,
       error: error => console.log(error),
       complete: () => console.log('complete')

6079136423eafeef204ebc6721b346548b7896c8
Ashish Singh <ashishsingh.9x@gmail.com>
2025-11-02 19:11:54 +0530

    Product detail form design including textbox design fix on global level.

--- client/src/app/features/shop/product-details/product-details.component.ts
+++ client/src/app/features/shop/product-details/product-details.component.ts
@@ -2,10 +2,16 @@ import { Component, inject, OnInit } from '@angular/core';
 import { ShopService } from '../../../core/services/shop.service';
 import { ActivatedRoute } from '@angular/router';
 import { Product } from '../../../shared/models/product';
+import { CurrencyPipe } from '@angular/common';
+import { MatAnchor, MatButton } from "@angular/material/button";
+import { MatIcon } from "@angular/material/icon";
+import { MatFormField, MatLabel } from "@angular/material/form-field";
+import { MatInput } from "@angular/material/input";
+import { MatDivider } from '@angular/material/divider';
 
 @Component({
   selector: 'app-product-details',
-  imports: [],
+  imports: [CurrencyPipe, MatAnchor, MatIcon, MatFormField, MatLabel, MatInput, MatDivider, MatButton ],
   templateUrl: './product-details.component.html',
   styleUrl: './product-details.component.scss',
 })

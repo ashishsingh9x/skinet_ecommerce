830d315c619ecafa26210fc9ea952ea5de7492c4
Ashish Singh <ashishsingh.9x@gmail.com>
2025-11-02 17:07:49 +0530

    Showing product detail page(component) by id on clicking of particular product, showing cursor on hover of product.

--- client/src/app/core/services/shop.service.ts
+++ client/src/app/core/services/shop.service.ts
@@ -32,6 +32,10 @@ export class ShopService {
     return this.http.get<Pagination<Product>>(this.baseUrl + 'products', { params });
   }
 
+  getProduct(id: number) {
+    return this.http.get<Product>(this.baseUrl + 'products/' + id);
+  }
+
   // as services are singleton, and components get disposed when we have routing, we will 
   // call & store brands and types only once.
   getBrands() {
--- client/src/app/features/shop/product-item/product-item.component.ts
+++ client/src/app/features/shop/product-item/product-item.component.ts
@@ -4,10 +4,11 @@ import { MatCard, MatCardContent, MatCardActions } from "@angular/material/card"
 import { CurrencyPipe } from '@angular/common';
 import { MatIcon } from "@angular/material/icon";
 import { MatAnchor } from "@angular/material/button";
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-product-item',
-  imports: [MatCard, MatCardContent, CurrencyPipe, MatCardActions, MatIcon, MatAnchor],
+  imports: [MatCard, MatCardContent, CurrencyPipe, MatCardActions, MatIcon, MatAnchor, RouterLink],
   templateUrl: './product-item.component.html',
   styleUrl: './product-item.component.scss',
 })

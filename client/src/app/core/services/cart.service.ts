2d85aa271e55e5ca24f2718e0f88ca2b9353470b
Ashish Singh <ashishsingh.9x@gmail.com>
2025-11-04 13:50:20 +0530

    Created environment files, Added get and set cart using signal in cart service.

--- client/src/app/core/services/cart.service.ts
+++ client/src/app/core/services/cart.service.ts
@@ -1,8 +1,26 @@
-import { Injectable } from '@angular/core';
+import { inject, Injectable, signal } from '@angular/core';
+import { environment } from '../../../environments/environment';
+import { HttpClient } from '@angular/common/http';
+import { Cart } from '../../shared/models/cart';
 
 @Injectable({
   providedIn: 'root',
 })
 export class CartService {
-  
+  baseUrl = environment.apiUrl;
+  private http = inject(HttpClient);
+  cart = signal<Cart | null>(null);
+
+  getCart(id: string) {
+    this.http.get<Cart>(this.baseUrl + 'cart?id=' + id).subscribe({
+        next: cartResponse => this.cart.set(cartResponse) //setting cart response to signal
+      }
+    )
+  }
+
+  setCart(cart: Cart) {
+    this.http.post<Cart>(this.baseUrl + 'cart', cart).subscribe({
+        next: cartResponse => this.cart.set(cartResponse)
+    })
+  }
 }

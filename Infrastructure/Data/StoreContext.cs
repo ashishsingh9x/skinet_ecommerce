3cd81ce4d03c81a3d83858f7c0ddf4635d561081
Ashish Singh <ashishsingh.9x@gmail.com>
2025-11-05 17:04:46 +0530

    Adding a user address class

--- Infrastructure/Data/StoreContext.cs
+++ Infrastructure/Data/StoreContext.cs
@@ -12,6 +12,7 @@ namespace Infrastructure.Data
         }
 
         public DbSet<Product> Products { get; set; }
+        public DbSet<Address> Addresses { get; set; }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
